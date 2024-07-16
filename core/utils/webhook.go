a1e682dcacde7d6617419103108a5c1c8eef4b9f
RA341 <razerishi1505@gmail.com>
2024-07-16 12:46:18 -0400

    fix: added logging to webhook

--- core/utils/webhook.go
+++ core/utils/webhook.go
@@ -1,8 +1,8 @@
 package utils
 
 import (
+	"github.com/rs/zerolog/log"
 	"io"
-	"log"
 	"net/http"
 	"strings"
 )
@@ -15,30 +15,30 @@ func SendWebHook(message []byte) bool {
 	req, err := http.NewRequest("POST", webhookUrl, payload)
 	req.Header.Add("Content-Type", "application/json")
 	if err != nil {
-		log.Println("Failed to create request.Reason: " + err.Error())
+		log.Error().Err(err).Msg("Failed to create request")
 		return false
 	}
 
 	res, err := http.DefaultClient.Do(req)
 
 	if err != nil {
-		log.Println("Failed to send request\nReason: " + err.Error())
+		log.Error().Err(err).Msg("Failed to send request")
 		return false
 	}
 
 	defer func(Body io.ReadCloser) {
 		err := Body.Close()
 		if err != nil {
-			log.Println("Failed to close buffer body")
+			log.Error().Err(err).Msg("Failed to close body")
 			return
 		}
 	}(res.Body)
 
 	if res.StatusCode < 300 {
-		log.Print("Sent to webhook")
+		log.Debug().Msgf("Webhook sent: %s", res.Status)
 		return true
 	} else {
-		log.Println("Request failed: " + res.Status)
+		log.Error().Msgf("Request failed: %s", res.Status)
 		return false
 	}
 }
