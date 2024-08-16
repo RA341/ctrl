244872ebc6fd16a937109713af257edb85c228e1
RA341 <razerishi1505@gmail.com>
2024-08-15 23:11:29 -0400

    fix: removed client

--- client/lib/services/channel/web.dart
+++
@@ -1,7 +0,0 @@
-import 'package:grpc/grpc_web.dart';
-
-GrpcWebClientChannel createChannel(String baseUrl, String port) {
-  print('init web');
-
-  return GrpcWebClientChannel.xhr(Uri.parse('http://$baseUrl:$port'));
-}
